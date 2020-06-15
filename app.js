754b9ab056ca9823b9391cb2ab9483c1cb4d992c
sabashahid786 <sabashahid4846@gmail.com>
2020-06-15 23:02:22 +0500

    java

---
+++ app.js
@@ -0,0 +1,157 @@
+// alert("hello world")
+// alert("Error! please enter a valid password.")
+// alert ('Welcome to JS Land... \n Happing Coding!')
+// alert ("Welcome to JS Land...")
+// alert  (' Happing Coding! \n prevent This page form creating additional dialogs')
+// alert ("Hello.... I can run jd thorugh my website browser's console")
+
+// var name="Jhone Doe"
+// alert(name)
+// var age="15 year's old"
+// alert(age)
+// var developer="certified Mobile Application Development"
+// alert(developer)
+// var name="pizza\npizz\npiz\npi\np"
+// alert(name)
+// var ="my email address is example@example.com"
+// alert(email)
+//  var book ="i am trying to learn form the book A smart \n way to learn javascript"
+//  alert(book)
+// document.write("Yah! I can write HTML content through javascript")
+//  var design="---------------------------ஜ۩۞۩ஜ---------------------------"
+//  alert(design)
+
+// var age ="I am 15 years old"
+// alert(age)
+// var time ="You have visited this site 14 times"
+//  alert(time)
+// document.write("My birth year is 1998")
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+// var name="majid"
+// var lastname ="shahid"
+// var age="21"
+// document.write(name +" "+ lastname +" "+ age)
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("<p>Variable names can only contain use space , mathematical operation ,logical operationand  ,or*or </p>")
+// document.write("<p> Variables must begin with a not, donot , cannot</p>")
+// document.write("<p> Variable names are case sensitive</p>")
+// document.write("<p> Variable names should not be JS keyword</p>")
+// document.write("sum of 3 and 5 is  ")
+//  var add = 3+5;
+//  document.write(add)
+//  document.write("sum of 3 and 5 is  ")
+//  var subtract = 3-5;
+//  document.write(subtract)
+//  document.write("sum of 3 and 5 is  ")
+//  var multiply= 3*5;
+//  document.write(multiply)
+// document.write("sum of 3 and 5 is  ")
+//  var division = 3/5;
+//  document.write(division)
+//  document.write("modulus")
+//  var modulus = 3-(5*5);
+//  document.write(modulus)
+// var a =5;
+// // document.write(a)
+// var b = ++a ;
+// document.write( "  value after incresment:" + b )
+// var c=7;
+//  var s =b+c
+//  document.write = (" value after addition:" + s)
+
+
+// var e =--s;
+// document.write("  value after decreament:" + e )
+
+//  document.write=( "the Remainder is : o ")
+// var a =600;
+// var b =a*5;
+
+// document.write("Total cost to buy 5 ticket to amovie is " + b)
+// document.write("table of 4"+ "<br>" )
+//  for (var i=1; i<=10 ; i++) 
+//  document.write("4" + "x" + i +"=" + 4*i + "<br>")
+//  var a=650;
+//  var a1=a*3;
+// var c =100;
+// var c1=c*8;
+// var d =a1+c1;
+
+
+//  document.write("<h1>Shopping Cart</h1>" + "<br>")
+// document.write("Price of item 1 is " + a + "<br>" )
+// document.write("Quantity of item 1 is 3" + "<br>")
+// document.write("Price of item 2 is " + c + "<br>" )
+// document.write("Quantity of item 2 is 7" + "<br>")
+// document.write("shipping charges 100")
+// document.write("Total cost of your order is " + d )
+
+
+// var a =980;
+// var b = 804;
+// var c =b / a *100;
+// document.write("<h1>Marks Sheet</h1>" + "<br>")
+// document.write("Total marks :" + a  + "<br>" )
+// document.write("Marks Obtained :" + b +"<br>")
+// document.write("Percentage:" + c + "%"  )
+// document.write("<h1>Currency in PKR</h1>" + "<br>")
+
+
+// var a=10*104.80;
+// var b =25*28;
+// var c =a+b;
+// document.write("Total currency in PKR :" + c )
+
+// var a=1;
+// var b =5;
+// var c =a+b;
+// var d = 10;
+// var e =c*d;
+// var f = 2;
+// var g = e / f;
+// document.write(g)
+
+// var a=2020;
+// var b =1998;
+// var c = a-b;
+// document.write("<h1>Age Calculater</h1>" + "<br>")
+// document.write("Current Year :" + a  + "<br>")
+// document.write("Birth Year :" + b  + "<br>")
+// document.write("Your Age is :" + c )
+// document.write("<h1>Result:</h1>" + "<br>")
+// var a=10;
+// document.write("The value of a is :" + a  + "<br>")
+// var b = ++a;
+// document.write("The value of ++a is :" + b + "<br>")
+// document.write("Now the value of a is :" + b + "<br>")
+// var c=b++;
+// document.write("The value of ++a is :" + c + "<br>")
+// document.write("Now the value of a is :" + b + "<br>")
+// var d = b-- ;
+// document.write("The value of --a is :" + b + "<br>")
+// document.write("Now the value of a is :" + b + "<br>")
+
+// var a= 2;
+// var b =1;
+// var result =--a - --b + ++b + b--;
+// document.write("a is:" + a+ "<br>")
+// document.write("b is:" + b+ "<br>")
+// document.write("result is:" + result+ "<br>")
+
+//  var a =prompt("Enter user name");{
+//      alert ("welcome")
+//  }
+
+// document.write("<h1>Subject Total Marks Obtained Marks Percentage</h1>" + "<br>")
+// document.write("English" +"" +"100" +""+ "54" +""+"54%"+ "<br>")
+// document.write("Math" +"" +"100" +""+ "54" +""+"54%"+ "<br>")
+// document.write("Urdu" +"" +"100" +""+ "48" +""+"48%"+ "<br>")
+// document.write("" +"" +"300" +""+ "156" +""+"52%")
+
+// var city = +prompt("enter your city name") 
+//  if (city === karachi) {
+//         alert("welcome the  city of light ")
+// }
+// else{
+//         alert("you are not write correct information")
+//     }
